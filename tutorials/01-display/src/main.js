5d874a97eea2c955da0601ee534c0866d2abcc64
inhibitor1217 <cytops1217@gmail.com>
2019-04-13 21:16:08 +0900

    configured basics

---
+++ tutorials/01-display/src/main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const gl = canvas.getContext('webgl');
+
+if (gl) {
+
+
+
+} else {
+    console.log('WebGL not supported in this browser.');
+}
---
+++ tutorials/01-display/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            
+        ]
+    }
+}
