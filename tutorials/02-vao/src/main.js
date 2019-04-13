271131470347de3b31815bd540385ba0ed67803d
inhibitor1217 <cytops1217@gmail.com>
2019-04-14 01:11:54 +0900

    initialize tutorial 2

---
+++ tutorials/02-vao/src/main.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById('canvas');
+const gl = canvas.getContext('webgl');
+
+if (gl) {
+
+    /* glViewPort(x, y, width, height)
+     * Specifies the affine transform from normalized device coordinates
+     * to window coordinates. */
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    /* Initialize frame buffer with color (0, 0, 0, 1). */
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+} else {
+    console.log('WebGL not supported in this browser.');
+}
