0e3928ed2a82db9b09f71325b1f259e952428ac9
inhibitor1217 <cytops1217@gmail.com>
2019-04-15 03:29:58 +0900

    wrote Program class

--- tutorials/02-vao/src/main.ts
+++ tutorials/02-vao/src/main.ts
@@ -1,51 +1,14 @@
 import Mesh from './engine/components/Mesh';
+import Program from './engine/shaders/Program';
+import { vertexShader, fragmentShader } from './engine/res/DefaultMaterialShaders';
 
 const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('canvas');
 const gl: WebGL2RenderingContext = canvas.getContext('webgl2');
 
-function createShader(gl: WebGL2RenderingContext, type: GLenum, source: string): WebGLShader | null {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-        return shader;
-    else {
-        console.log(gl.getShaderInfoLog(shader));
-        gl.deleteShader(shader);
-        return null;
-    }
-}
-
-function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram | null {
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (gl.getProgramParameter(program, gl.LINK_STATUS))
-        return program;
-    else {
-        console.log(gl.getProgramInfoLog(program));
-        gl.deleteProgram(program);
-        return null;
-    }
-}
-
-const vs = `
-    attribute vec2 position;
-
-    void main() { gl_Position = vec4(position, 0.0, 1.0); }
-`
-
-const fs = `
-    void main() { gl_FragColor = vec4(1, 1, 1, 1); }
-`
-
 if (gl) {
 
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vs);
-    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fs);
-    const program = createProgram(gl, vertexShader, fragmentShader);
-    gl.bindAttribLocation(program, 0, 'position');
+    const program = new Program(gl, vertexShader, fragmentShader);
+    program.generate();
 
     /* glViewPort(x, y, width, height)
      * Specifies the affine transform from normalized device coordinates
@@ -66,15 +29,15 @@ if (gl) {
     mesh.setNumVertices(6);
     mesh.generate();
 
-    gl.useProgram(program);
-
     gl.bindVertexArray(mesh.getVAO());
     gl.enableVertexAttribArray(0);
+    
+    program.start();
     gl.drawArrays(mesh.getDrawMode(), 0, mesh.getNumVertices());
+    program.stop();
+    
     gl.disableVertexAttribArray(0);
     gl.bindVertexArray(null);
-
-    gl.useProgram(null);
 
     mesh.release();
 
